fc6b22c91c3cb0e0594ea43927efd713895edd99
Brandon Monier <brandon.monier@gmail.com>
2019-04-09 23:04:03 -0400

    Add sub plot options

--- R/plot_all_palettes.R
+++ R/plot_all_palettes.R
@@ -2,14 +2,36 @@
 #'
 #' @description Plots all croix palettes simultaneously.
 #'
+#' @param palType Which palette type should be plotted? Choices are
+#'    \code{all} which plots all palette types, \code{brand} which plots
+#'    only brand-type palettes, \code{croix} which plots only croix-type
+#'    palettes, or \code{mov} which plots only movie-based palettes. Defaults
+#'    to \code{all}.
 #' @param ... Additional parameters to pass to inner functions.
 #'
 #' @importFrom graphics plot rect text
 #'
 #' @export
-croix_all <- function(...) {
-    names <- names(croix_pal_data)
-    max_len <- max(lengths(croix_pal_data))
+croix_all <- function(palType = c("all", "brand", "croix", "mov"), ...) {
+
+    palType <- match.arg(palType)
+
+    if (palType == "all") {
+        main_title <- "All palettes"
+        palType <- "*"
+    } else if (palType == "brand") {
+        main_title <- "Brand palettes"
+    } else if (palType == "croix") {
+        main_title <- "Croix palettes"
+    } else if (palType == "mov") {
+        main_title <- "Movie palettes"
+    }
+
+    croix_sub <- croix_pal_data[grepl(palType, names(croix_pal_data))]
+
+    names <- names(croix_sub)
+    max_len <- max(lengths(croix_sub))
+
     graphics::par(mar = c(0.5, 12, 2, 1))
 
     graphics::plot(
@@ -19,7 +41,7 @@ croix_all <- function(...) {
         axes = FALSE,
         xlab = "",
         ylab = "",
-        main = "All palettes"
+        main = main_title
     )
 
     for (i in seq_len(max_len)) {
